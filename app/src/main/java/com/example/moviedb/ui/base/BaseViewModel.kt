c064798fdf07c36f59aca1678d29ef736b00a059
khanhpt96 <khanh281096@gmail.com>
2019-04-11 08:37:22 +0700

    fix coroutine

--- app/src/main/java/com/example/moviedb/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/example/moviedb/ui/base/BaseViewModel.kt
@@ -3,61 +3,55 @@ package com.example.moviedb.ui.base
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.moviedb.utils.LoadType
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 abstract class BaseViewModel<Item>() : ViewModel() {
     val isLoading = MutableLiveData<Boolean>()
-    val errorLoading = MutableLiveData<String>()
+    val errorMessage = MutableLiveData<String>()
     val movies = MutableLiveData<ArrayList<Item>>()
     var listMovie = ArrayList<Item>()
     val isLoadingMore = MutableLiveData<Boolean>()
 
     //coroutines
-    val parentJob = Job()
+    val parentJob = SupervisorJob()
     val ioContext = parentJob + Dispatchers.IO
     val uiContext = parentJob + Dispatchers.Main
     val ioScope = CoroutineScope(ioContext)
     val uiScope = CoroutineScope(uiContext)
 
-    val compositeDisposable = CompositeDisposable()
-
-    fun addDisposable(disposable: Disposable) {
-        compositeDisposable.add(disposable)
+    val handler = CoroutineExceptionHandler { _, throwable ->
+        uiScope.launch {
+            onLoadFail(throwable)
+        }
     }
+
+    val ioScopeError = CoroutineScope(ioContext + handler)
+    val uiScopeError = CoroutineScope(uiContext + handler)
 
     fun showLoading() {
         isLoading.value = true
-        isLoading.postValue(true)
     }
 
     fun hideLoading() {
-        isLoading.postValue(false)
-        isLoadingMore.postValue(false)
         isLoading.value = false
         isLoadingMore.value = false
     }
 
     fun showLoadingMore() {
         isLoadingMore.value = true
-        isLoadingMore.postValue(true)
     }
 
     fun showError(e: Throwable) {
-        errorLoading.value = e.message
+        errorMessage.value = e.message
     }
 
     fun onDestroy() {
-        compositeDisposable.clear()
         parentJob.cancel()
     }
 
     suspend fun onLoadSuccess(moviesResponse: List<Item>?, type: LoadType) {
-        hideLoading()
+        withContext(uiContext) {
+            hideLoading()
             if (type == LoadType.MORE) {
                 listMovie = if (movies.value != null) {
                     movies.value!!
@@ -68,14 +62,14 @@ abstract class BaseViewModel<Item>() : ViewModel() {
                 listMovie = ArrayList()
             }
             listMovie.addAll(moviesResponse ?: listOf())
-            movies.postValue(listMovie)
+            movies.value = listMovie
+        }
     }
 
     suspend fun onLoadFail(throwable: Throwable) {
-        hideLoading()
-        errorLoading.value = throwable.toString()
-        withContext(uiContext){
-            onLoadFail(throwable)
+        withContext(uiContext) {
+            hideLoading()
+            errorMessage.value = throwable.toString()
         }
     }
 }
