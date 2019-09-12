aacf0d881fbc433c728e5eeb1ab428c4917f6106
pham.tien.khanh <SUN-ASTERISK\pham.tien.khanh@b121761-pc.sun-asterisk.com>
2019-09-12 16:36:51 +0700

    coil-kt

--- app/src/main/java/com/example/moviedb/utils/Constants.kt
+++ app/src/main/java/com/example/moviedb/utils/Constants.kt
@@ -3,5 +3,5 @@ package com.example.moviedb.utils
 object Constants {
     const val DATABASE_NAME = "DB_MOVIE"
     const val PAGE = "page"
-    const val BASE_IMAGE_URL = "http://image.tmdb.org/t/p/w342/"
+    const val BASE_IMAGE_URL = "http://image.tmdb.org/t/p/original/"
 }
